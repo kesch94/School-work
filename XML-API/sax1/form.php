28a8b82c53700fc6d2b63dfdfca306206d1fb586
kesch94 <c20antka@student.his.se>
2021-04-26 12:26:10 +0200

    asd

--- XML-API/sax1/form.php
+++ XML-API/sax1/form.php
@@ -1,42 +1,44 @@
 <html>                                                                                                                                                
 <body>                                                                                                                                                
 <form method="POST" action="response.php" >
-<select name="choise">
-<option value="none">Morning_Edition article</option>                                                                                                                                                 
-<?php                                                                                                                                                 
-     
-function startElement($parser, $entityname, $attributes) {
-	if($entityname=="NEWSPAPER"){
-		echo "<option value='".$attributes['TYPE']."'>".$attributes['NAME'];
-	}
+<select name="Newspaper">
+<option value="none">Morning_Edition articles</option>  
+<?php
 
-}
+    function startElement($parser, $entity, $attributes) {
+    echo $entity;
+        if($entity=="NEWSPAPER"){
+            echo "<option value='".$attributes['TYPE']."'>";
+            echo $attributes['NAME'];
+        }
+    }
+    
+    function endElement($parser, $entity) {
+        if($entity=="NEWSPAPER"){
+            echo "</option>";
+        }
+    }
+    
+    function charData($parser, $chardata) {
+    }
+    
+    $parser = xml_parser_create();
+    xml_set_element_handler($parser, "startElement", "endElement");
+    xml_set_character_data_handler($parser, "charData");
+    
+    $url="https://wwwlab.iit.his.se/gush/XMLAPI/articleservice/papers";
+    $data = file_get_contents($url);
+    
+    if(!xml_parse($parser, $data, true)){
+    printf("<P> Error %s at line %d</P>", xml_error_string(xml_get_error_code($parser)),xml_get_current_line_number($parser));
+    }else{
+    // print "<br>Parsing Complete!</br>";
+    }
+    
+    xml_parser_free($parser);
+    
 
-function endElement($parser, $entityname) {
-	if($entityname=="NEWSPAPER"){
-		echo "</option>";
-	}
-}
-
-function charData($parser, $chardata) {
-}
-
-$parser = xml_parser_create();
-xml_set_element_handler($parser, "startElement", "endElement");
-xml_set_character_data_handler($parser, "charData");
-
-$url="https://wwwlab.iit.his.se/gush/XMLAPI/articleservice/papers";
-$data = file_get_contents($url);
-
-if(!xml_parse($parser, $data, true)){
-printf("<P> Error %s at line %d</P>", xml_error_string(xml_get_error_code($parser)),xml_get_current_line_number($parser));
-}else{
-//print "<br>Parsing Complete!</br>";
-}
-
-xml_parser_free($parser);
- 
-?> 
+?>
 </select>
 <button>Search</button>                                                                                                                                                   
 </form>                                                                                                                                                
--- XML-API/sax1/response.php
+++ XML-API/sax1/response.php
@@ -1,6 +1,9 @@
-<html>  
-<head>
-	<style>
+<html>
+<html lang="en">
+    <head>
+        <meta chartset="UTF-8">
+        <title>XML SAX</title>
+        <style>
 		@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 		
 		table,tbody,td,td{
@@ -44,103 +47,105 @@
 		}
 
 	</style>
-</head>                                                                                                                                              
-<body>
+    </head>
+    <body>
 <a href="form.php">Back</a>
-<table>                                                                                                                                                
+<table border='1' style="border-collapse: collapse; border: solid 2px black">
 <?php
-$lastelement="";                                                                                                                                              
-function startElement($parser, $entityname, $attributes) {
-	global $lastelement;
-	
-	if($entityname=="NEWSPAPER"){
-		echo "<tr class='rad'>";
-		echo "<td>".$attributes['NAME']."</td>";
-		echo "<td>".$attributes['SUBSCRIBERS'];
-		echo "<br>".$attributes['TYPE']."</td>";
-	}
-	else if($entityname=="ARTICLE"){
-		echo "<td>";
-		if($attributes['DESCRIPTION']=="News"){
-			echo "<table class='new'>";
-		}
-		else if($attributes['DESCRIPTION']=="Review"){
-			echo "<table class='rev'>";
-		}
-		else{
-			echo "<table>";
-		}
-		echo "<tr>";		
-		echo "<td>".$attributes['ID']."</td>";
-		if(isset($attributes['TIME'])){
-			echo "<td>".$attributes['TIME']."</td>";
-		}		
-		echo "<td>".$attributes['DESCRIPTION']."</td>";
-		echo "</tr>";	
-	}	
-	else if($entityname=="HEADING"){
-		echo "<tr class='head'>";		
-		echo "<td>";
-		echo "<h3>";
-		$lastelement=$entityname;
-	}	
-	else if($entityname=="STORY"){
-		echo "<tr class='story'>";
-		echo "<td>";
-		echo "<div>";
-		$lastelement=$entityname;
-	}
-}
 
-function endElement($parser, $entityname) {
-	if($entityname=="NEWSPAPER"){
-		echo "</td></tr>";
-	}
-	else if($entityname=="ARTICLE"){
-		echo "</table></td>";
-	}
-	else if($entityname=="HEADING"){
-		echo "</h1></td></tr>";
-	}
-	else if($entityname=="STORY"){
-		echo "</div></td></tr>";
-	}
-}
+$globalE="";
 
-function charData($parser, $chardata) {
-	global $lastelement;
-	
-	if($lastelement=="STORY"){
-		echo "<p>".$chardata."</p>";
-	}
-	else{
-		echo $chardata;
-	}
-}
-
-$parser = xml_parser_create();
-xml_set_element_handler($parser, "startElement", "endElement");
-xml_set_character_data_handler($parser, "charData");
-
-if(isset($_POST['choise'])&&($_POST['choise']!="none")){
-	$type=$_POST['choise'];
-	$url="https://wwwlab.iit.his.se/gush/XMLAPI/articleservice/articles?paper=".$type;
-	$data = file_get_contents($url);
-}
-else{
-	$url="https://wwwlab.iit.his.se/gush/XMLAPI/articleservice/articles?paper=Morning_Edition";
-	$data = file_get_contents($url);
-}
-
-if(!xml_parse($parser, $data, true)){
-printf("<P> Error %s at line %d</P>", xml_error_string(xml_get_error_code($parser)),xml_get_current_line_number($parser));
+if(isset($_POST['NEWSPAPER'])){
+    $newspaper=$_POST['NEWSPAPER'];
 }else{
-// print "<br>Parsing Complete!</br>";
+    $newspaper="Morning_Edition";
 }
 
-xml_parser_free($parser);
- 
+foreach($_POST as $newspaper);
+
+$url="https://wwwlab.iit.his.se/gush/XMLAPI/articleservice/articles?paper=".$newspaper;
+$data = file_get_contents($url);
+
+echo "<tr><th>Newspaper</th><th>Subscribers</th><th>Type</th><th>Article</th></tr>";
+function startElement($parser, $entity, $attributes) {
+    global $globalE;
+
+        if($entity=="NEWSPAPER"){
+            echo "<tr>";
+            foreach ($attributes as $attname => $attvalue) {
+                echo "<td style='background:grey;' >";
+                echo "<div>";
+                echo $attvalue;
+                echo "</div>";
+                echo "</td>";
+            }
+            echo "<td><table>";   
+        }
+        else if($entity=="ARTICLE"){
+            foreach ($attributes as $attname => $attvalue) {    
+                if($attvalue=='News'){
+                    echo "<td style='background:beige;' >";
+                    echo $attvalue;
+                    echo "</td>"; 
+                }else{
+                    echo "<td style='background:lightblue;' >"; 
+                    echo $attvalue;
+                    echo "</td>"; 
+                }
+            }
+            echo "</tr>";
+        }
+        else if($entity=="HEADING"){
+            echo "<td colspan='3'>";
+            echo "<h3>";
+        }
+        else if($entity=="STORY"){
+            echo "<div>";
+        }
+
+        if($entity!="TEXT") $globalE=$entity;
+}
+
+    function endElement($parser, $entity) {
+        if($entity=="NEWSPAPER"){
+            echo "</table></td>";
+            echo "</tr>";
+        }else if($entity=="ARTICLE"){
+            echo "</td></tr>";     
+        }else if($entity=="STORY"){
+            echo "</div>";
+        }else if($entity=="HEADING"){
+            echo "</h3>";
+        }           
+    }
+    
+    function charData($parser, $chardata) {
+        global $globalE;
+
+        $chardata=trim($chardata);
+        if($chardata=="") return;
+        if($globalE=="STORY"){
+            echo"<p style='color:darkgrey; font-family:sans-serif;'>".$chardata."</div>";
+        }else{
+            echo $chardata;
+        }  
+    }
+    
+    $parser = xml_parser_create();
+    xml_set_element_handler($parser, "startElement", "endElement");
+    xml_set_character_data_handler($parser, "charData");
+    
+
+    
+    if(!xml_parse($parser, $data, true)){
+    printf("<P> Error %s at line %d</P>", xml_error_string(xml_get_error_code($parser)),xml_get_current_line_number($parser));
+    }else{
+    // print "<br>Parsing Complete!</br>";
+    }
+    
+    xml_parser_free($parser);
+
 ?>
-</table>                                                                                                                                                                                                                                                                                                   
-</body>                                                                                                                                               
+</table>
+</body>
 </html>
