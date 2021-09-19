43bcd0594304a3a0c0b53b0d5828685b2ea6b8a1
kesch94 <73368418+kesch94@users.noreply.github.com>
2021-09-19 20:16:20 +0200

    Add files via upload

---
+++ OLS with R.R
@@ -0,0 +1,18 @@
+
+library(tidyverse)
+library(lmtest)
+library(AER)
+library(car)
+library(MASS)
+library(ggplot2)
+data("CPS1988")
+help("CPS1988")
+str(CPS1988)
+head(CPS1988)
+with(data = CPS1988, boxplot(wage ~ education))
+with(data = CPS1988, boxplot(log(wage) ~ education))
+ggplot(data = CPS1988, mapping = aes(x = factor(education), y = log(wage))) +
+  geom_boxplot() +
+  theme_minimal()
+data_cps <- with(data = CPS1988,
+                 subset(x = CPS1988, subset = experience > 1 & education > 1))
