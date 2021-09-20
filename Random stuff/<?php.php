1bb652a5bcee55d3c747a0b2b5b66edc1a151bd8
kesch94 <73368418+kesch94@users.noreply.github.com>
2021-09-20 21:53:32 +0200

    update

---
+++ Random stuff/<?php.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1)
+error_reporting(E_ALL);
+
+$cookiepath = "/tmp/cookies.txt";
+$tmeout=3600; // (3600=1hr)
+
+$baseurl = 'https://projectcompany.erpnext.com';
+
+try{
+    $ch = curl_init($baseurl.'api/method/login');
+}
+catch (Exception $e) {
+    echo 'Caught exception: ', $e->getMessage(), "\n";
+}
+
+curl_setopt($ch,CURLOPT_POST, true);
+
+curl_setopt($ch,CURLOPT_POSTFIELD, '{"usr":"c20antka@student.his.se","pwd":"*********")}');
+
+?>
